ee573b81f0955378011da1733f90a8e5fbf531df
FireFrame-Loader <74542308+FireFrame-Loader@users.noreply.github.com>
2020-11-18 19:05:12 +0100

    Command processing

--- API/Loader/handler.php
+++ API/Loader/handler.php
@@ -7,4 +7,24 @@ if($_SERVER['HTTP_USER_AGENT'] !== '#F1R3FR4M3L04D3R#'){
     ]));
 }
 
+if (!isset($_POST['command'])) {
+    die(json_encode([
+        'error' => true,
+        'type' => 'no_command_requested'
+    ]));
+}
+
+$command = $_POST['command'];
+
+switch ($command) {
+    default:
+    die(json_encode([
+        'error' => true,
+        'type' => 'unknown_command'
+    ]));
+    break;
+}
+
+?>
+
 
