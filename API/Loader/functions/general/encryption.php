4b8067effb1cd4ca56134779c9c05250a392f8b3
FireFrame-Loader <74542308+FireFrame-Loader@users.noreply.github.com>
2020-11-27 12:05:35 +0100

    errors

---
+++ API/Loader/functions/general/encryption.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+
+Include phpseclib (http://phpseclib.sourceforge.net/rsa/2.0/examples.html), i had problems doing it on live server
+see /api.firefra.me/error.log thanks
+
+*/
+namespace general;
+
+use phpseclib\Crypt\RSA;
+
+function decrypt($data) {
+
+}
+
+
+
+
+?>
